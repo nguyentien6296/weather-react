c5e42216f76a5847b7cda1fed11a31511d4c4bd7
nguyentien6296 <88523384+nguyentien6296@users.noreply.github.com>
2022-11-06 17:04:10 +0700

    Changed the loader to blue

---
+++ src/Weather.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import { ThreeCircles } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}℃`
+    );
+  }
+  let apiKey = "1c2d25d3b778335dacad0d1205b20d4e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <ThreeCircles
+      height="100"
+      width="100"
+      color="blue"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel="three-circles-rotating"
+      outerCircleColor=""
+      innerCircleColor=""
+      middleCircleColor=""
+    />
+  );
+}
